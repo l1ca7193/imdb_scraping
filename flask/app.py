0505bad966d157906f44135d9679de930308392c
l1ca7193 <66485972+l1ca7193@users.noreply.github.com>
2021-12-09 17:10:18 +0100

    Pulizia file

--- flask/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from views import views
-
-app = Flask(__name__)
-#accessing the roots from "/"
-app.register_blueprint(views, url_prefix="/")
-
-if __name__ == '__main__':
-    app.run(debug=False, port=8000)
