6f0fc409b70879f889bfe6bbc6652cb5e6f266a9
brodo97 <brodo97nosense@gmail.com>
2021-12-13 12:37:42 +0100

    Some code cleanup and improvements, I guess

--- googler_imdb.py
+++ googler_imdb.py
@@ -3,20 +3,17 @@ import re
 from random import randint
 import time
 
-with open("registi.list","r") as f:
-        lines = f.readlines()
-        for line in lines:
-                regista = line.strip()
-                query = subprocess.run(["googler", "-n", "1", "imdb.com", regista, "--json"], capture_output=True, text=True)
-                query = query.stdout
-                code = re.findall("nm\d{1,7}",query)
-                try:
-                        print(regista + " " + code[0])
-                        with open("imdb_urls.txt", "a+") as ff:
-                                ff.write(regista+","+"https://www.imdb.com/name/"+code[0]+"\n")
-                except:
-                        print("The query is empty")
-                # TIME SLEEP IS A MUST OR GOOGLE WILL SEND YOU TO HORNY JAIL
-                stop = randint(100,200)
-                print("I'm sleeping for "+str(stop)+"s")
-                time.sleep(stop)
+with open("Input/Directors.list", "r") as _R:
+    with open("Output/imdb_urls.txt", "w") as _W:
+        for line in _R:
+            director = line.strip()
+            query = subprocess.run(["googler", "-n", "1", "imdb.com", director, "--json"], capture_output=True, text=True)
+            query = query.stdout
+            code = re.findall("nm\d{1,7}",query)
+            print(f"{director}: {code[0]}")
+            _W.write(f"{director},https://www.imdb.com/name/{code[0]}\n")
+
+            # TIME SLEEP IS A MUST OR GOOGLE WILL SEND YOU TO HORNY JAIL
+            wait = randint(100,200)
+            print(f"I'm sleeping for {wait} seconds")
+            time.sleep(wait)
--- soup_imdb.py
+++ soup_imdb.py
@@ -5,57 +5,65 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import time
-import datetime
-import numpy
-import pandas
+import csv
 
 films = []
 
-begin_time = datetime.datetime.now()
-print(datetime.datetime.now())
-with open("imdb_codes.txt","r") as f:
-    lines = f.readlines()
-    for id, line in enumerate(lines):
-        begin_time = datetime.datetime.now()
-        codiceRegista = line.strip()
-        URL = "https://www.imdb.com/name/" + codiceRegista
+csv_file = open("Output/films226_directors.csv", "w", encoding="utf8", newline="\n")
+csv_writer = csv.writer(csv_file, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
+
+csv_writer.writerow(['DirectorName', 'DirectorCode', 'MovieName', 'MovieCode', 'Score', 'Year'])
+
+with open("Input/imdb_codes.txt", "r") as _R:
+    for line_id, line in enumerate(_R):
+        begin_time = time.time()
+        director_code = line.strip()
+        URL = "https://www.imdb.com/name/" + director_code
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, "html.parser")
-        regista = soup.find("title").get_text()
-        regista_clean = re.sub(" - IMDb", "", regista)
+        director_name = soup.find("title").text
+        director_name = re.sub(" - IMDb", "", director_name)
 
-        ii = 1
-        while ii < 3:
-            startURL = "https://www.imdb.com/filmosearch/?explore=title_type&role="
-            midURL = "&ref_=filmo_ref_typ&mode=simple&page="
-            endURL = "&sort=user_rating,desc&title_type=movie"
-            
-            URL = startURL + codiceRegista + midURL + str(ii) + endURL 
-            page = requests.get(URL)
+        page_index = 1
+        while "Looking for movies":
+            page = requests.get(
+                url='https://www.imdb.com/filmosearch?',
+                params={"explore": "title_type",
+                    "role": director_code,
+                    "ref_": "filmo_ref_typ",
+                    "mode": "simple",
+                    "page": page_index,
+                    "sort": "user_rating,desc",
+                    "title_type": "movie"}
+            )
 
             soup = BeautifulSoup(page.content, "html.parser")
+            movies = soup.find_all("div", {"class": "lister-item"})
 
-            page_coltitle = soup.find_all("div", class_="col-title")
-            page_colimdbrating = soup.find_all("div", class_="col-imdb-rating")
+            if len(movies) == 1 and "No results." in movies[0].text:
+                break
 
-            for md, ti, ye in zip(page_colimdbrating, page_coltitle, page_coltitle):
+            for movie in movies:
                 try:
-                    score_strong = md.find("strong").get_text()
-                    score = score_strong.replace(" ","")
+                    details = movie.find("div", {"class": "lister-item-image"}).a.img
+                    title = details["alt"]
+                    movie_code = details["data-tconst"]
+                    score = movie.find("div", {"class": "col-imdb-rating"}).text.strip()
+                    score = score.replace(",", ".")
+                    year = movie.find("span", {"class": "lister-item-year"}).text
+                    year = re.findall("\d{4}", year)
 
-                    year_span= ye.find("span", class_="lister-item-year text-muted unbold").get_text()
-                    year = re.findall("\d{4}", str(year_span))
+                    if score == "-":
+                        score = "Unknown"
+                    if year == []:
+                        year = ["Unknown"]
 
-                    filmname = ti.find("a").get_text()
-                    title_code = re.findall("tt\d{1,7}", str(ti))
-                    if score_strong != "IMDbRating":
-                        films.append([regista, codiceRegista, filmname, title_code[0], score.strip(), year[0]])           
-                    else:
-                        pass
+                    csv_writer.writerow([director_name, director_code, title, movie_code, score, year[0]])
+                    csv_file.flush()
                 except Exception as error:
+                    print(error)
                     pass
-            time.sleep(6)
-            ii+=1
-        print(str(id+1)+ " " + codiceRegista + " " + str(datetime.datetime.now() - begin_time))
+            time.sleep(2)
+            page_index += 1
+        print(f"{line_id + 1}) {director_name} ({director_code}) took {time.time() - begin_time:.1f} seconds")
 
-pandas.DataFrame(films).to_csv('films226_directors.csv', index_label = "Index", header  = ['director name','director code','film name','title code','score', 'year'])    
